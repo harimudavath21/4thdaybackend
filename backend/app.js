fe8b7ca6e46d9225cba2c3bdac8dda5027dcabb6
harimudavath21 <hariprasad4876@gmail.com>
2025-08-19 09:13:32 +0000

    completed express setup

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+import express from "express"
+const app = express();
+const PORT = 5000;
+;
+app.get("/",(req,res)=>{
+    res.send("Welcome to Express ES6 setup");
+});
+app.get("/get",(req,res)=>{
+    res.json({message:"This is a GET route example"})
+});
+app.listen(PORT,() => {
+    console.log(`✅server running at http://localhost:${PORT}`);
+});
